a6c4350a6bbc269d11ae532d297351f7fb5684b6
hanpan <hanpan@163.com>
2026-03-05 19:41:28 +0800

    Add ProgressData.cs file

---
+++ Models/ProgressData.cs
@@ -0,0 +1,13 @@
+// Add your C# code here
+using System;
+
+namespace Models
+{
+    public class ProgressData
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public int Score { get; set; }
+    }
+}
